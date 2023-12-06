f1f6144d618e2b33b1115c4c7e6374b8abed7d2f
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-12-06 13:37:51 -0500

    tagline component complete

---
+++ src/components/tagline/statsParagraph/StatsParagraph.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './statsParagraph.css'
+
+function StatsParagraph() {
+  return (
+    <div className='textContainer'>
+
+        <h2 className='largeText'>When a billion people go hungry each day,<br></br>
+
+   
+       how could we spend another dollar on war?</h2>
+  
+       <h3 className='smallText'>
+       Food Not Bombs is an all-volunteer movement that recovers food that would otherwise be discarded. Our movement has no headquarters or positions of leadership {`-`} instead, we use the process of consensus to make decisions. 
+       </h3>
+    </div>
+  )
+}
+
+export default StatsParagraph
