29952a6a12d3e76c9b39b7936700d2a779962131
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-12-06 15:53:30 -0500

    all content added to divs for resources, working on responsiveness

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import {Route, Routes} from 'react-router-dom'
 import HomePage from './pages/Home/HomePage';
 import Navbar from './components/shared/Navbar';
 import PlaceholderPage from './pages/Placeholder/PlaceholderPage'
+import Tagline from './components/tagline/Tagline';
+import Resources from './components/resources/Resources';
 
 
 function App() {
 
   return (
     <Fragment>
+      <Tagline />
+      <Resources/>
       <Navbar /> 
       <Routes>
         <Route
