4a11a79885784232795f42cdac8f352ae9da29f5
danielamansolino <danielamosqueragranda@gmail.com>
2023-12-07 18:11:40 -0500

    OurHistory component complete - Timeline carousel complete

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import AboutMainVideo from '../../components/about/AboutMainVideo';
+import OurHistory from '../../components/about/OurHistory';
 
 function About(props){
 
     return(
         <div>
             <AboutMainVideo/>
+            <OurHistory/>
         </div>
 
     );
