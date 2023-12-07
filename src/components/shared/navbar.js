bbad3afafb0bd9fda9f64fa7eef6b0dce8eb99eb
danielamansolino <danielamosqueragranda@gmail.com>
2023-12-07 10:48:28 -0500

    Donate link update

--- src/components/shared/navbar.js
+++ src/components/shared/navbar.js
@@ -1,51 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './Navbar.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import {
-//   Navbar,
-//   Nav,
-//   NavItem,
-//   NavLink,
-// } from 'reactstrap';
-
-// function NavBar(props) {
-//   return (
-//     <Navbar expand="md" className="custom-navbar">
-//       <Nav className="mr-auto">
-//         <NavItem>
-//           <img className="logo" src={'https://imgur.com/Dv5KY2h.png'} alt={'FNB LOGO'} />
-//         </NavItem>
-//         <NavItem>
-//           <img className="fnb" src={'https://imgur.com/Fa1q8d0.png'} alt={'FOOD NOT BOMBS'} />
-//         </NavItem>
-//         <NavItem>
-//         <NavLink tag={Link} to="/" className="nav-link" activeClassName="active">HOME</NavLink>
-//         </NavItem>
-//         <NavItem>
-//           <NavLink tag={Link} to="/about" activeClassName="selected">ABOUT</NavLink>
-//         </NavItem>
-//         <NavItem>
-//           <NavLink tag={Link} to="/location" activeClassName="selected">LOCATION</NavLink>
-//         </NavItem>
-//         <NavItem>
-//           <NavLink tag={Link} to="/resources" activeClassName="selected">RESOURCES</NavLink>
-//         </NavItem>
-//         <NavItem>
-//           <NavLink tag={Link} to="/volunteer" activeClassName="selected">VOLUNTEER</NavLink>
-//         </NavItem>
-//       </Nav>
-//       <Nav className="ml-auto">
-//         <NavItem>
-//           <img className="donate" src={'https://imgur.com/4OrYh2K.png'} alt={'DONATE'} />
-//         </NavItem>
-//       </Nav>
-//     </Navbar>
-//   );
-// }
-
-// export default NavBar;
-
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './navbar.css';
@@ -100,9 +52,13 @@ function NavBar(props) {
       <div className="mobile-menu-icon" onClick={handleMobileMenuToggle}>
         ☰
       </div>
-      <div className="donate-container">
+        <a href="https://foodnotbombs.net/new_site/donate.php" target="_blank">
         <img className="donate" src={'https://imgur.com/4OrYh2K.png'} alt={'DONATE'} />
-      </div>
+        </a>
+
+      {/* <div className="donate-container">
+        <img className="donate" src={'https://imgur.com/4OrYh2K.png'} alt={'DONATE'} />
+      </div> */}
     </nav>
   );
 }
