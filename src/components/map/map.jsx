cf251e63148ce0aef6df41d4f11fce3f84442fac
Noah Smith <noahcsmith1993@gmail.com>
2023-12-07 14:46:35 -0500

    add initial location page and make map size customizable

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './map.css'
 
 function Map(props) {
+   const { width, height } = props
+
     return (
         <div className='mapbox'>
             <iframe
                 title="Food Not Bombs Location Map"
                 src="https://www.google.com/maps/d/u/0/embed?mid=1EwOJB5w4Wqzyt8tO2lehHtm4lq8GFgY&ehbc=2E312F"
-                width="1280"
-                height="900"
+                width={width}
+                height={height}
                 style={{ border:0 }}
                 allowfullscreen=""
                 loading="lazy"
