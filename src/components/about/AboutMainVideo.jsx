afd000f179faf284561677ade2aed4aa7745e3c1
Amber Jankowski <97934285+AmJann@users.noreply.github.com>
2023-12-07 12:04:44 -0500

    aboutMainVideo component added with h1 title and video embedded

---
+++ src/components/about/AboutMainVideo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function AboutMainVideo() {
+  return (
+    <div>
+        <h1 className='title'>40+ Years of Food Not Bombs</h1>
+        <div className='videoContainer'>
+            <iframe width="854" height="480" src="https://www.youtube.com/embed/bjEOXjnfouk"
+             title="Food Not Bombs 35th Anniversary" frameborder="0" 
+             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+  )
+}
+
+export default AboutMainVideo
