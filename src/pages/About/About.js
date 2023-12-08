7c9c4df65cd7bc90d3b098ef981efe3b80e4594b
danielamansolino <danielamosqueragranda@gmail.com>
2023-12-07 22:17:48 -0500

    Guiding Principles component complete

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutMainVideo from '../../components/about/AboutMainVideo';
 import OurHistory from '../../components/about/OurHistory';
+import GuidingPrinciples from '../../components/about/GuidingPrinciples'
 
 function About(props){
 
     return(
         <div>
             <AboutMainVideo/>
+            <GuidingPrinciples/>
             <OurHistory/>
         </div>
 
